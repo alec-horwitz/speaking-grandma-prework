f438e08ec9626f58b12db07224d2346887a9bacd
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-01-27 20:54:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(string)
   if string == string.upcase
     string = "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!" || "I LOVE YOU GRANDMA!"
+  elsif string == "I LOVE YOU GRANDMA!" || string == "I LOVE YOU GRANDMA!"
     string = "I LOVE YOU TOO PUMPKIN!"
   else
     string = "HUH?! SPEAK UP, SONNY!"
